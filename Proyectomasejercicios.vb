58578d4ed740cc14151aa4e6c4c2c7347fe9f9cf
Adrián Iglesias Fernández <72018196+igleepuntoexe@users.noreply.github.com>
2021-11-17 00:39:03 +0100

    Delete Proyectomasejercicios.vb

--- Proyectomasejercicios.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class Form5
-    Private Sub BotonPulsar_Click(sender As Object, e As EventArgs) Handles BotonPulsar.Click
-        Informacion.Text = "Tu nombres es: " + nombreIntroducirtxt.Text + " / Tu correo es: " + IntroducetuCorreo.Text
-    End Sub
-End Class
